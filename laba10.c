184595e786c667e456b40ad91fc9ff4f44b1a8f2
Egrlove <maf2514@gmail.com>
2015-11-23 18:20:05 +0300

    Непонятная жуьб

--- laba10.c
+++ laba10.c
@@ -1,9 +1,28 @@
 
-
+#define BUF 81
 #include <stdio.h>
 
+void spacerm (char *buffer)
+{int i=0,simvol;
+ do
+ {
+  simvol = buffer[i];
+  if (simvol == '	' || simvol == ' ')
+  {
+   printf("Razdel '%c'\n",simvol ); 
+  }
+  i++;
+ }while (simvol != '\0');
+	
+}
+
 int main()
-{
- char buffer[81];
+{ 
+ char buffer[BUF];
+
+ gets(buffer);
+ spacerm(buffer);
+
+ printf("%s\n",buffer);
 
 }
