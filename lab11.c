9da12d6299f1d502b122808b663f8ae88e068c91
Egrlove <maf2514@gmail.com>
2015-12-02 00:31:34 +0300

    Final 11

--- lab11.c
+++ lab11.c
@@ -1,20 +1,16 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 #define BUF 40
 
-int words (char charcter)
+int words (char character)
 {
  static int simvl; 
  int raz = 0;
-  switch (charcter)
- {
-   case 9 : case 10 : case 11 : case 32 :
-     raz = 1; break; 
-   default :
-     raz = 0; break;
- }
+ if (isspace(character))
+ 	raz = 1;
+
  if (simvl > 0 && raz > 0)
   {
    simvl = 0;
@@ -24,15 +20,13 @@ int words (char charcter)
  return 0;
 }
 
-struct new_word
-{
+struct new_word {
  char *only_word;
- struct new_word *previous;
-};
+ struct new_word *previous; };
 
-void out (struct new_word *current, FILE *fp)
+void output (struct new_word *current, FILE *fp)
 {
-	    struct new_word *p;
+ struct new_word *p;
  for (p = current; p != NULL; p = p -> previous)
 	{
 	 fprintf(fp,  "%s\n",p -> only_word );
@@ -45,31 +39,35 @@ int main(int argc, char const *argv[])
  FILE *fp;
  char buffer[BUF];
  int  i = 0  ;
- char charcter, *for_word ;
+ char character, *for_word ;
  struct new_word *for_struct , *current = NULL;
-   if ( (fp = fopen(argv[1], "w+" )) == NULL )
-      {	printf("Error\n"); return 0;}
+ if ( (fp = fopen(argv[1], "w+" )) == NULL )
+      {	printf("Не удалось открыть файл\n"); exit (1);}
    
- while ((charcter = getchar()) != EOF)
+ while ((character = getchar()) != EOF)
    {
-    if ( words(charcter) != 1)
+    if ( words(character) != 1 && i < BUF -1)
       {
-       if (isalpha(charcter))
-        buffer[i++] = charcter;
+       if (isalpha(character))
+        buffer[i++] = character;
       }
     else 
       {
-       buffer[i] = '\0';
-       if ( (for_word = (char*)malloc( sizeof(char) * i)) == NULL)
-	      { printf("Error\n"); return 0; }
-	   strcpy(for_word , buffer);
-	   i =0;
-	   if ( (for_struct = malloc( sizeof(struct new_word))) == NULL )
-	      {	printf("Error\n"); return 0; }
-       for_struct -> only_word = for_word;
-       for_struct -> previous  = current;
-       current = for_struct;
+       if (i > 0)
+         {
+         buffer[i] = '\0';
+         if ( (for_word = (char*)malloc( sizeof(char) * i))== NULL)
+	        { printf("Error\n");  exit (1); }
+	     strcpy(for_word , buffer);
+	     i =0;
+	     if ( (for_struct = malloc( sizeof(struct new_word)))==NULL)
+	        { printf("Error\n");  exit (1); }
+         for_struct -> only_word = for_word;
+         for_struct -> previous  = current;
+         current = for_struct;
+        }
       }
    }
-out(current, fp);
+output(current, fp);
+fclose(fp);
 }
