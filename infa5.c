e8a1b7d96a1077a4abfe92f892f07fa6d7fff2ea
Egrlove <maf2514@gmail.com>
2015-12-08 18:41:31 +0300

    Улучшенная версия

--- infa5.c
+++ infa5.c
@@ -1,23 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 64
 
+FILE* opening (const char *name, int num, char* type)
+{FILE* help;
+ if ((help = fopen(name, type)) == NULL)
+   {
+    printf("Ошибка в файле номер: #%d \n", num);
+    exit(num);
+   } 
+ else
+ 	return help;
+}
 int main(int argc, char const *argv[])
 {
- unsigned int wintable[64], i, c;
+ unsigned int wintable[SIZE], i, c;
  FILE *code_table, *tex_koi , *to;
- if (argc < 3)
+ if (argc > 3)
  {
-   if ((code_table = fopen(argv[1], "r")) == NULL)
-     {
-      printf("Oshibka\n"); exit(1);
-     } 
+   code_table = opening (argv[1], 1, "r");
+   tex_koi = opening (argv[2], 2, "r");
+   to =  opening (argv[3], 3, "w+");
   
-   for (i = 0; i < 64; ++i)
+   for (i = 0; i < SIZE; ++i)
      { 
    	  fscanf(code_table, "%x" , &wintable[i]);
      } 
-   tex_koi = fopen(argv[2], "r");
-   to =  fopen(argv[3], "w+");
 
    while ((c = fgetc(tex_koi)) != EOF)
      {
@@ -27,4 +35,6 @@ int main(int argc, char const *argv[])
         fputc(wintable[c - 192],to);
      }
  }
+ else
+ 	printf("Нет фалов\n");
 }
