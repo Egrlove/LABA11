14b2640ab55c6736e2b9eac8c60d9a2bdb69d2e5
Egrlove <maf2514@gmail.com>
2015-12-16 00:07:35 +0300

    Final

--- rait3.c
+++ rait3.c
@@ -1,5 +1,10 @@
+/*
+Разработайте программу, которая вводит со стандартного потока ввода числа
+и помещает их в линейный односвязный список так,чтобы элементы его составляли возрастающую последовательность.
+Затем из исходного списка сформировать два списка: первый будет содержать четные элементы исходного списка, второй - нечетные.
+Числа из этих списков (исходный, первый, второй) записать в файл, имя которого определяется в командной строке .
+*/
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 #define  TRUE 1
@@ -15,24 +20,23 @@ typedef struct lis
 }list;
 
 list *first_list_beginning = NULL;
- FILE *go;
-
-
 
 int getnum (char );  
-void doing_list (int);
-list* forming_list (int);
-void output (list* ,int);
+void doing_first_list (int);
+list* forming_different_lists (int);
+void output (list* ,int, FILE*);
+list* memory_for_element(void);
 
 int main(int argc, char const *argv[])
 {
+ FILE *go;
  int digit, c;
- list* sec_list_start = NULL ,* third_list_start= NULL;
+ list* second_list_start = NULL ,* third_list_start= NULL;
  if (argc > 1) 
    {
-    if ((go = fopen (argv[1], "w+"))== NULL)
+    if ((go = fopen (argv[1], "w"))== NULL)
      {
-      printf("Ошибка открытия файла.\n");
+      printf("Ошибка при открытии файла.\n");
       exit(1);
      }
    }
@@ -49,32 +53,35 @@ int main(int argc, char const *argv[])
    	if ((digit = getnum(c) )!= NOTANUM )
    	  {
    	   fprintf(go,"%10d",digit );
-       doing_list(digit);
+       doing_first_list(digit);
    	  }// end of if
    }while (c != EOF);
  if (first_list_beginning == NULL)
-   fprintf(go, "входные данные отсутвуют \n" );
- sec_list_start = forming_list(EVEN);
- third_list_start = forming_list(ODD);
- output(first_list_beginning, 1);
- output(sec_list_start, 2);
- output(third_list_start, 3);
+   fprintf(go, "входные данные отсутствуют \n" );
+ second_list_start = forming_different_lists(EVEN);
+ third_list_start = forming_different_lists(ODD);
+ output(first_list_beginning, 1, go);
+ output(second_list_start, 2, go);
+ output(third_list_start, 3, go);
 
  fclose(go);
 }
+//  END
+//  OF 
+//  MAIN
 
+list* memory_for_element (void)
+{
+ list *element;
+ if (( element = (list *)malloc(sizeof(list)) ) == NULL )
+   {
+   	printf("Не удалось создать элемент списка\n");
+   	exit(1);
+   }
+ return element;
+}//end of func
 
-
-
-
-
-
-
-
-
-
-
-void output (list *current , int number)
+void output (list *current , int number, FILE *go)
 {
  int t =0;
  list *p , *help;
@@ -85,11 +92,11 @@ void output (list *current , int number)
 	 fprintf(go , "Число: %d \n", p -> num );
 	 help = p -> next;
 	 free(p);
-	}
-}
+	}//end of for
+}//end of func
 
 
-list* forming_list (int chetnost)
+list* forming_different_lists (int chetnost)
 {
  char NOT_END = 1;
  list *p, *for_struct, *list_start;
@@ -99,7 +106,7 @@ list* forming_list (int chetnost)
    {
    	if (p -> num %2 == chetnost)
    	  {
-   	   list_start = malloc(sizeof(list));
+   	   list_start = memory_for_element();
    	   list_start -> num = p -> num ;
    	   for_struct = list_start;
    	   NOT_END = 0;
@@ -110,7 +117,7 @@ list* forming_list (int chetnost)
    {
    	if (p -> num %2 == chetnost)
    	  {
-       for_struct -> next = malloc(sizeof(list));
+       for_struct -> next = memory_for_element();
        for_struct = for_struct -> next;
        for_struct -> num = p -> num;
    	  }//end of if
@@ -119,17 +126,17 @@ list* forming_list (int chetnost)
  return list_start;
 }//end of func
 
-void doing_list ( int digit)  
+void doing_first_list ( int digit)  
 {
  list *prev = NULL, *for_struct, *p;
  if (first_list_beginning == NULL)
    { 
-    first_list_beginning = malloc(sizeof(list));
+    first_list_beginning = memory_for_element();
     first_list_beginning -> num = digit;
     first_list_beginning -> next = NULL;
     return;
    }
- for_struct = malloc(sizeof(list));
+ for_struct = memory_for_element();
  for_struct -> num = digit;
  for_struct -> next = NULL;
  p = first_list_beginning;
